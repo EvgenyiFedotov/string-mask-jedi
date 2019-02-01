5011ad0cdf3a226207b80fd6d403e662feda2e51
efedotov <evgeniy.fedotov@bia-tech.ru>
2019-02-01 16:38:57 +0300

    combine add tests

--- spec/combine.spec.js
+++ spec/combine.spec.js
@@ -13,43 +13,39 @@ const tests = {
       { value: '7', cursor: 1 },
       { value: '+7', cursor: 2, space: ' (   )    -  -  ', isMatched: true },
     ],
+    [
+      { value: '+', cursor: 1 },
+      { value: '', cursor: 0, space: '', isMatched: true },
+    ],
+    [
+      { value: '9888777', cursor: 7 },
+      { value: '+9 888 777', cursor: 10, space: '', isMatched: true },
+    ],
+    [
+      // ? Может быть баг с курсором, пока оставим так (редкий кейс)
+      { value: '+79 888 777', cursor: 2 },
+      { value: '+7 (988) 877-7', cursor: 5, space: ' -  ', isMatched: true },
+    ],
+  ],
+
+  remove: [
+    [
+      { value: '+7 (988) 877-7', cursor: 14 },
+      { value: '+7 (988) 877-7', cursor: 14, space: ' -  ', isMatched: true },
+    ],
+    [
+      { value: '+ (988) 877-7', cursor: 1 },
+      { value: '+9 888 777', cursor: 2, space: '', isMatched: true },
+    ],
+    [
+      { value: '+9 8 777', cursor: 4 },
+      { value: '+9 877 7', cursor: 4, space: '', isMatched: true },
+    ],
+    [
+      { value: '+', cursor: 1 },
+      { value: '', cursor: 0, space: '', isMatched: true },
+    ],
   ],
 };
 
 runTests(configMask, tests, combine);
-
-// test('simple', () => {
-//   const combineMask = combine(
-    
-//   );
-//   let res = null;
-
-//   res = combineMask({
-//     value: '7',
-//     cursor: 1,
-//   });
-
-//   console.log('@res', res);
-
-//   res = combineMask({
-//     value: '+',
-//     cursor: 1,
-//   });
-
-//   console.log('@res', res);
-
-//   res = combineMask({
-//     value: '9888777',
-//     cursor: 7,
-//   });
-
-//   console.log('@res', res);
-
-
-//   res = combineMask({
-//     value: '+79 888 777',
-//     cursor: 2,
-//   });
-
-//   console.log('@res', res);
-// });
