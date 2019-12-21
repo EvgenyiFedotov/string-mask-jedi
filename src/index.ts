17c3d226dda42a24962177905fcb3554477e53f0
e.fedotov <fedotov1290@gmail.com>
2019-12-21 17:46:12 +0300

    feat: add build cursor

--- src/index.ts
+++ src/index.ts
@@ -2,65 +2,108 @@ interface State {
   value: string;
   currValue: string;
   nextValue: string[];
+  cursor: number;
+  nextCursor: number;
 }
 
-type ConfigElement = (state: State, index: number) => State | void;
+type ConfigElement = (
+  state: State,
+  prevState: State,
+  index: number,
+) => State | void;
+
+interface UseMatchOptions {
+  replace?: string;
+  defaultValue?: string;
+  additional?: true;
+}
+
+const createEmptyValue = (config: ConfigElement[]) => config.map(() => "");
 
 export const createMask = (...config: ConfigElement[]) => {
-  let currState: State = { value: "", currValue: "", nextValue: [] };
+  let currState: State = {
+    value: "",
+    cursor: 0,
+    currValue: "",
+    nextValue: createEmptyValue(config),
+    nextCursor: 0,
+  };
+  let prevState: State = { ...currState };
 
-  return (value: string) => {
-    currState = { value, currValue: value, nextValue: [] };
+  return (value: string, cursor: number = 0) => {
+    currState = {
+      value,
+      cursor,
+      currValue: value,
+      nextValue: createEmptyValue(config),
+      nextCursor: 0,
+    };
 
     for (let index = 0; index < config.length; index += 1) {
-      const nextState = config[index](currState, index);
+      const nextState = config[index](currState, prevState, index);
 
       if (!nextState || nextState === currState) break;
       currState = nextState;
     }
 
+    prevState = { ...currState };
+
     return { ...currState, nextValue: currState.nextValue.join("") };
   };
 };
 
-interface UseMatchOptions {
-  replace?: string;
-  defValue?: string;
-}
-
 export const useMatch = (
-  getMatch: (state: State) => RegExp | string,
+  getMatch: (state: State, prevState: State) => RegExp,
   options: UseMatchOptions = {},
-): ConfigElement => (state) => {
-  const { replace = "", defValue } = options;
-  const match = getMatch(state);
-  let reg: RegExp;
-
-  if (typeof match === "string") {
-    reg = new RegExp(match.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"));
-  } else {
-    reg = match;
-  }
-
-  const matchResult = state.currValue.match(reg);
+): ConfigElement => (state, prevState, index) => {
+  const { replace = "", defaultValue, additional } = options;
+  const match = getMatch(state, prevState);
+  const matchResult = state.currValue.match(match);
 
   if (matchResult) {
     const nextState = { ...state };
 
-    nextState.nextValue.push(matchResult[0]);
     nextState.currValue = nextState.currValue.replace(match, replace);
 
+    if (additional) {
+      if (nextState.currValue) {
+        nextState.nextValue[index] = matchResult[0];
+
+        if (state.cursor - 1 === index) {
+          nextState.nextCursor = state.cursor + 1;
+        } else if (state.cursor > index) {
+          nextState.nextCursor = state.cursor;
+          nextState.cursor += 1; // TODO use another name
+        }
+      } else {
+        if (state.cursor - 1 === index) {
+          nextState.nextCursor = state.cursor - 1;
+        }
+      }
+    } else {
+      nextState.nextValue[index] = matchResult[0];
+
+      if (state.cursor - 1 === index) {
+        nextState.nextCursor = state.cursor;
+      }
+    }
+
     return nextState;
-  } else if (typeof match === "string") {
+  } else if (defaultValue) {
     const nextState = { ...state };
 
-    nextState.nextValue.push(match);
+    if (additional) {
+      if (nextState.currValue) {
+        nextState.nextValue[index] = defaultValue;
 
-    return nextState;
-  } else if (defValue) {
-    const nextState = { ...state };
-
-    nextState.nextValue.push(defValue);
+        if (state.cursor - 1 === index) {
+          nextState.nextCursor = state.cursor + 1;
+        } else if (state.cursor > index) {
+          nextState.nextCursor = state.cursor + 1;
+          nextState.cursor += 1; // TODO use another name
+        }
+      }
+    }
 
     return nextState;
   }
