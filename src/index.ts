beebc87c3bdf852b8857cf9ffbd55a7f1956e02d
e.fedotov <fedotov1290@gmail.com>
2019-12-31 20:11:02 +0300

    feat: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-export { createMask, Token, State, GetMatch } from "./create-mask";
+export {
+  createMask,
+  Token,
+  State,
+  GetMatch,
+  Mask,
+  MaskResult,
+} from "./create-mask";
 export { TokenConfig, Config, Converter } from "./create-config";
